ab5fda5494cd6112a5a0be4f920e616209394e00
Beadsley <danbeadleson@gmail.com>
2020-04-23 20:32:56 +0200

    Pop over added

--- client/src/components/AppBar.js
+++ client/src/components/AppBar.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import {AppBar, Toolbar, Typography, IconButton, InputBase} from '@material-ui/core';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  InputBase,
+  Popover,
+} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import InfoIcon from '@material-ui/icons/Info';
 import GitHubIcon from '@material-ui/icons/GitHub';
@@ -62,11 +69,38 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     marginLeft: 'auto',
   },
+  popover: {
+    pointerEvents: 'none',
+  },
+  paper: {
+    padding: theme.spacing(1),
+    backgroundColor: '#494949',
+  },
+  popText: {
+    fontSize: 12,
+    color: 'white',
+  },
 }));
 
 export default function SearchAppBar() {
   const classes = useStyles();
   const [userInput, setUserInput] = useState('');
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [popupMessage, setPopupMessage] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handlePopoverOpen = (event, icon) => {
+    setAnchorEl(event.currentTarget);
+    if (icon === 'info') {
+      setPopupMessage('Further information');
+    } else if (icon === 'github') {
+      setPopupMessage('GitHub repository');
+    }
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
 
   function keyPress(e) {
     if (e.keyCode == 13) {
@@ -79,7 +113,14 @@ export default function SearchAppBar() {
     <div className={classes.root}>
       <AppBar position='static'>
         <Toolbar>
-          <IconButton onClick={'handleClick'} color='inherit'>
+          <IconButton
+            onClick={'handleClick'}
+            color='inherit'
+            aria-owns={open ? 'mouse-over-popover' : undefined}
+            aria-haspopup='true'
+            onMouseEnter={(e) => handlePopoverOpen(e, 'github')}
+            onMouseLeave={handlePopoverClose}
+          >
             <GitHubIcon />
           </IconButton>
           {/* <Typography variant='h6' className={classes.title}>
@@ -104,11 +145,42 @@ export default function SearchAppBar() {
             className={classes.iconContainer}
             className={classes.icon}
           >
-            <IconButton onClick={'handleClick'} color='inherit'>
+            <IconButton
+              onClick={'handleClick'}
+              color='inherit'
+              fontSize='large'
+              aria-owns={open ? 'mouse-over-popover' : undefined}
+              aria-haspopup='true'
+              onMouseEnter={(e) => handlePopoverOpen(e, 'info')}
+              onMouseLeave={handlePopoverClose}
+            >
               <InfoIcon />
             </IconButton>
           </div>
         </Toolbar>
+        <Popover
+          id='mouse-over-popover'
+          className={classes.popover}
+          classes={{
+            paper: classes.paper,
+          }}
+          open={open}
+          anchorEl={anchorEl}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          onClose={handlePopoverClose}
+          disableRestoreFocus
+        >
+          <Typography className={classes.popText}>
+            {popupMessage}
+          </Typography>
+        </Popover>
       </AppBar>
     </div>
   );
