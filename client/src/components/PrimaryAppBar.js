e37faa3c0fd666bd9c8860b1e7d917cffd437b8a
Beadsley <danbeadleson@gmail.com>
2020-04-25 10:11:48 +0200

    Search bar refactored to seperate functional component

--- client/src/components/PrimaryAppBar.js
+++ client/src/components/PrimaryAppBar.js
@@ -1,56 +1,16 @@
 import React from 'react';
-import { fade, makeStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, IconButton, InputBase } from '@material-ui/core';
-import SearchIcon from '@material-ui/icons/Search';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, IconButton } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
 import GitHubIcon from '@material-ui/icons/GitHub';
 import config from '../config';
 import InfoDialog from './InfoDialog';
 import Iconpopover from './Iconpopover';
-import { useDispatch } from 'react-redux';
-import { addUsername } from '../actions/userActions';
-import { languageStatsLoading, hasErrored } from '../actions/languageStatsActions';
+import SearchBar from "./SearchBar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(9),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
   },
   infoIconContainer: {
     flex: 1,
@@ -67,11 +27,9 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SearchAppBar() {
+export default function PrimaryAppBar() {
   const classes = useStyles();
-  const dispatch = useDispatch();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [userInput, setUserInput] = React.useState('');
   const [popoverMessage, setPopoverMessage] = React.useState(undefined);
   const [showInfo, setShowInfo] = React.useState(false);
   const open = Boolean(anchorEl);
@@ -89,16 +47,6 @@ export default function SearchAppBar() {
   const handlePopoverClose = () => setAnchorEl(null);
   const handleInfoOpen = () => setShowInfo(true);
   const handleInfoClose = () => setShowInfo(false);
-  const handleUserInput = (e) => setUserInput(e.target.value);
-
-  function keyPress(e) {
-    if (e.keyCode === 13) {
-      dispatch(languageStatsLoading(true));
-      dispatch(hasErrored(false));
-      dispatch(addUsername(e.target.value));
-      setUserInput('');
-    }
-  }
 
   return (
     <div className={classes.root}>
@@ -116,22 +64,7 @@ export default function SearchAppBar() {
               <GitHubIcon className={classes.icon} />
             </IconButton>
           </a>
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-            <InputBase
-              value={userInput}
-              placeholder='Search…'
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-              onKeyDown={keyPress}
-              onChange={handleUserInput}
-            />
-          </div>
+          <SearchBar />
           <div className={classes.infoIconContainer} className={classes.infoIcon}>
             <IconButton
               onClick={handleInfoOpen}
