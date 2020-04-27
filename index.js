d532ab5b45bc3d2af37c44a96af91d0a7f35a893
Beadsley <danbeadleson@gmail.com>
2020-04-27 14:38:25 +0200

    Refactored

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/api', routes);
-
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
-  });
-}
+process.env.NODE_ENV === 'production' && app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.listen(PORT, () => {
   console.log(`running om port: ${PORT}`);
