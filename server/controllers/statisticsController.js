f14d5b0a55152d82d057863d898997e42b53c6c2
Natalie Yilmaz <natalieyilmaz@Natalies-MacBook-Pro.local>
2020-07-04 15:22:30 +0200

    Request made to github graphQL api

--- server/controllers/statisticsController.js
+++ server/controllers/statisticsController.js
@@ -1,10 +1,11 @@
 const { evalLanguages } = require('../services/statistics.js');
-const { getGithubRepoData, getLanguageData, getUserInfo } = require('../services/api.js');
+const { getGithubRepoData, getLanguageData, getUserInfo, testy } = require('../services/api.js');
 const { ENUMS } = require('../config/config.js');
 
 const getLanguageStatistics = async (_root, args) => {
   try {
     const username = args.username;
+    testy();
     const user = await getUserInfo(username);
     const repos = await getGithubRepoData(username);
     if (repos.length === 0) throw new Error(ENUMS.ERRORMESSAGE.NO_REPOS);
--- server/services/api.js
+++ server/services/api.js
@@ -1,5 +1,64 @@
 const { request } = require('./httpClient.js');
 process.env.NODE_ENV === 'development' && require('dotenv').config();
+const { ApolloClient, HttpLink, InMemoryCache, split, gql } = require('@apollo/client');
+const fetch = require('cross-fetch');
+
+const { setContext } = require('apollo-link-context');
+// const { getMainDefinition } = require('@apollo/client/utilities');
+// const { WebSocketLink } = require('@apollo/link-ws');
+console.log(process.env.TOKEN);
+
+const authLink = setContext((_, { headers }) => {
+  const token = process.env.TOKEN;
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `bearer ${token}` : null,
+    },
+  };
+});
+// const wsLink = new WebSocketLink({
+//   uri: `https://api.github.com/graphql`,
+//   options: {
+//     reconnect: true,
+//   },
+// });
+
+const httpLink = new HttpLink({ uri: `https://api.github.com/graphql`, fetch });
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: authLink.concat(httpLink),
+});
+
+const query = gql`
+  {
+    user(login: "beadsley") {
+      name
+      repositories(first: 100) {
+        nodes {
+          databaseId
+          isPrivate
+          name
+          languages(first: 20, orderBy: { field: SIZE, direction: DESC }) {
+            edges {
+              size
+              node {
+                name
+              }
+            }
+            totalSize
+          }
+        }
+      }
+    }
+  }
+`;
+
+const testy = async () => {
+  const response = await client.query({ query });
+  console.log(response);
+};
 
 const getGithubRepoData = async (username) => {
   return await request({
@@ -22,7 +81,7 @@ const getLanguageData = (repos) => {
     });
   });
 };
-
+// https://api.github.com/graphql
 const getUserInfo = async (username) => {
   return await request({
     method: 'get',
@@ -36,4 +95,5 @@ module.exports = {
   getGithubRepoData,
   getLanguageData,
   getUserInfo,
+  testy,
 };
