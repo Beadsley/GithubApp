bbe53377a8abff05227e7eeca956beeeb8b00b42
Natalie Yilmaz <natalieyilmaz@Natalies-MacBook-Pro.local>
2020-07-05 10:07:18 +0200

    Refactored server side structure

--- server/index.js
+++ server/index.js
@@ -1,28 +1,7 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const { getLanguageStatistics } = require('./controllers/statisticsController');
+const { typeDefs } = require('./graphQL/schema');
 const PORT = process.env.PORT || 8080;
-
-const typeDefs = gql`
-  type Language {
-    name: String!
-    sum: Int!
-    fraction: Float!
-    label: String!
-  }
-  type Languages {
-    mostused: [Language!]!
-    additional: [Language!]!
-    total: Int!
-  }
-  type User {
-    name: String!
-    projects: Int!
-    languages: Languages!
-  }
-  type Query {
-    languageStatistics(username: String!): User
-  }
-`;
 
 const resolvers = {
   Query: {
--- server/services/httpClient.js
+++ server/services/httpClient.js
@@ -1,8 +1,8 @@
 process.env.NODE_ENV === 'development' && require('dotenv').config();
 const { ApolloClient, HttpLink, InMemoryCache } = require('@apollo/client');
 const fetch = require('cross-fetch');
-
 const { setContext } = require('apollo-link-context');
+
 const authLink = setContext((_, { headers }) => {
   const token = process.env.TOKEN;
   return {
